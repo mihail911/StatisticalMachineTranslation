5baf79923be21b7752c3bec85e0f17ce8224ecf6
Mihail Eric <meric@corn20.stanford.edu>
2014-09-27 16:26:47 -0700

    PMI model

---
+++ pa1/java/src/cs224n/wordaligner/PMIModel.java
@@ -0,0 +1,20 @@
+package cs224n.wordaligner;
+
+import cs224n.util.*;
+import java.util.List;
+
+/**
+ * Source file implementing a basic Pointwise mutual information model
+ * for word alignment.
+ */
+public class PMIModel implements WordAligner {
+
+	public Alignment align(SentencePair sentencePair){
+		return new Alignment();
+	}
+
+	public void train(List<SentencePair> trainingData){
+
+	}
+
+}
