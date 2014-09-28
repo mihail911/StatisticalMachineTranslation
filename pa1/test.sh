753026c2f73475fc429aeb1f6a5593d785a09b24
Victor Zhong <victor@victorzhong.com>
2014-09-27 21:55:32 -0700

    reorganizing test scripts

---
+++ pa1/test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+java -cp ~/cs224n/pa1/java/classes cs224n.assignments.WordAlignmentTester \
+  -dataPath /afs/ir/class/cs224n/data/pa1/ \
+  -model cs224n.wordaligner.PMIModel -evalSet dev \
+  -trainSentences 200
--- runtest.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-java -cp pa1/java/classes cs224n.assignments.WordAlignmentTester -dataPath /afs/ir/class/cs224n/data/pa1 -model cs224n.wordaligner.PMIModel -evalSet miniTest -verbose
