a1477eb16be83cfd49cec40c16d151833c7ffb6c
Victor Zhong <victor@victorzhong.com>
2014-09-28 21:26:47 -0700

    pushing model 2: same as model 1 currently

---
+++ pa1/java/src/cs224n/wordaligner/IBMModel2.java
@@ -0,0 +1,109 @@
+package cs224n.wordaligner;
+
+import cs224n.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+
+public class IBMModel2 implements WordAligner{
+
+  private Counter<String> foreignCount = new Counter<String>();
+  private CounterMap<String,String> coocurrenceCount =
+              new CounterMap<String,String>();
+  private CounterMap<String,String> translationProb =
+              new CounterMap<String,String>(); // This actually P(English | Foreign)
+
+  Integer numIterations = 5;
+
+  public Alignment align(SentencePair sentencePair){
+    return new Alignment();
+  }
+
+  public void train(List<SentencePair> trainingdata){
+
+    for (SentencePair pair: trainingdata) {
+      List<String> englishWords = pair.getTargetWords();
+      List<String> foreignWords = pair.getSourceWords();
+
+      foreignWords.add(0, NULL_WORD);
+
+      System.out.println(englishWords);
+      System.out.println(foreignWords);
+
+      for (String foreignWord: foreignWords) {
+        for (String englishWord: englishWords) {
+          translationProb.incrementCount(foreignWord, englishWord, 1.0);
+        }
+      }
+
+      foreignWords.remove(0);
+    }
+
+    Set<String> foreignWordsSet = translationProb.keySet();
+
+    for (String foreignWord: foreignWordsSet) {
+      Counter<String> englishWordCounters = translationProb.getCounter(foreignWord);
+      Double englishWordPairs = englishWordCounters.totalCount();
+      Set<String> englishWords = englishWordCounters.keySet();
+      for(String englishWord: englishWords) {
+        Double oldCount = englishWordCounters.getCount(englishWord);
+        translationProb.setCount(foreignWord, englishWord, oldCount/englishWordPairs);
+      }
+    }
+
+    for (int s = 0; s < numIterations; s += 1) {
+      // clear counts
+      coocurrenceCount = new CounterMap<String,String>();
+      foreignCount = new Counter<String>();
+
+      for (SentencePair pair: trainingdata) {
+        List<String> englishWords = pair.getTargetWords();
+        List<String> foreignWords = pair.getSourceWords();
+
+        foreignWords.add(0, NULL_WORD);
+
+        for (String foreignWord: foreignWords) {
+          for (String englishWord: englishWords) {
+
+            Double foreignWordsNormalizationCount = 0.0;
+            for (String foreignWordNested: foreignWords) {
+              foreignWordsNormalizationCount += translationProb.getCount(foreignWordNested, englishWord);
+              // System.out.println(englishWord + " | " + foreignWordNested + ": " + translationProb.getCount(foreignWordNested, englishWord));
+            }
+            Double delta = translationProb.getCount(foreignWord, englishWord) / foreignWordsNormalizationCount;
+
+            // System.out.println("F: " + foreignWord + " E: " + englishWord + " delta: " + delta + " = " + translationProb.getCount(foreignWord, englishWord) + "/" + foreignWordsNormalizationCount);
+
+            Double oldCount = coocurrenceCount.getCount(foreignWord, englishWord);
+            coocurrenceCount.setCount(foreignWord, englishWord, oldCount + delta);
+            oldCount = foreignCount.getCount(foreignWord);
+            foreignCount.setCount(foreignWord, oldCount + delta);
+          }
+        }
+
+        foreignWords.remove(0);
+      }
+
+      for (String foreignWord: foreignWordsSet) {
+        Counter<String> englishWordCounters = translationProb.getCounter(foreignWord);
+        Double englishWordPairs = englishWordCounters.totalCount();
+        Set<String> englishWords = englishWordCounters.keySet();
+
+        Double fCount = foreignCount.getCount(foreignWord);
+
+        for(String englishWord: englishWords) {
+          Double cCount = coocurrenceCount.getCount(foreignWord, englishWord);
+
+          translationProb.setCount(foreignWord, englishWord, cCount/fCount);
+        }
+      }
+
+      // System.out.println(translationProb);
+    }
+
+
+
+  }
+
+}
