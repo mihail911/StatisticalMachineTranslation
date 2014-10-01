7f07819f2c0daace13bdcd2f08df7a635dd2be0e
Mihail Eric <meric@corn20.stanford.edu>
2014-09-30 21:41:15 -0700

    modified debug

--- pa1/debug.sh
+++ pa1/debug.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 java -cp java/classes cs224n.assignments.WordAlignmentTester \
 -dataPath /afs/ir/class/cs224n/data/pa1/ \
--model cs224n.wordaligner.IBMModel1 -evalSet miniTest -verbose
+-model cs224n.wordaligner.IBMModel2 -evalSet miniTest -verbose
